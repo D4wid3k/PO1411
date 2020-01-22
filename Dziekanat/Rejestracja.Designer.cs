1bde9f59e124630f35b2a3af252729f0443bab95
Dawid Kosior <d4wt0n@gmail.com>
2020-01-22 18:28:59 +0100

    Nowe winforms

--- Dziekanat/Rejestracja.Designer.cs
+++ Dziekanat/Rejestracja.Designer.cs
@@ -29,15 +29,21 @@
         private void InitializeComponent()
         {
             this.RejestrHeader = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.LoginLabel = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.EmailTextbox = new System.Windows.Forms.TextBox();
+            this.ImieTextbox = new System.Windows.Forms.TextBox();
+            this.NazwiskoTextbox = new System.Windows.Forms.TextBox();
+            this.PeselTextbox = new System.Windows.Forms.TextBox();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.ImieLabel = new System.Windows.Forms.Label();
+            this.NazwiskoLabel = new System.Windows.Forms.Label();
+            this.PeselLabel = new System.Windows.Forms.Label();
+            this.HasloLabel = new System.Windows.Forms.Label();
+            this.HasloTextbox = new System.Windows.Forms.TextBox();
+            this.PunktyRekrLabel = new System.Windows.Forms.Label();
+            this.PunktyRekrTextbox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ZarejestrujButton = new System.Windows.Forms.Button();
+            this.Button = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // RejestrHeader
@@ -45,108 +51,177 @@
             this.RejestrHeader.AutoSize = true;
             this.RejestrHeader.Font = new System.Drawing.Font("Segoe UI Semibold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.RejestrHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.RejestrHeader.Location = new System.Drawing.Point(308, 21);
+            this.RejestrHeader.Location = new System.Drawing.Point(307, 37);
             this.RejestrHeader.Name = "RejestrHeader";
             this.RejestrHeader.Size = new System.Drawing.Size(182, 45);
             this.RejestrHeader.TabIndex = 2;
             this.RejestrHeader.Text = "Rejestracja";
             // 
-            // textBox1
+            // EmailTextbox
             // 
-            this.textBox1.Location = new System.Drawing.Point(305, 137);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(287, 22);
-            this.textBox1.TabIndex = 3;
+            this.EmailTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.EmailTextbox.Location = new System.Drawing.Point(366, 118);
+            this.EmailTextbox.Name = "EmailTextbox";
+            this.EmailTextbox.Size = new System.Drawing.Size(287, 30);
+            this.EmailTextbox.TabIndex = 3;
             // 
-            // textBox2
+            // ImieTextbox
             // 
-            this.textBox2.Location = new System.Drawing.Point(305, 235);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(287, 22);
-            this.textBox2.TabIndex = 4;
+            this.ImieTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ImieTextbox.Location = new System.Drawing.Point(366, 216);
+            this.ImieTextbox.Name = "ImieTextbox";
+            this.ImieTextbox.Size = new System.Drawing.Size(287, 30);
+            this.ImieTextbox.TabIndex = 4;
             // 
-            // textBox3
+            // NazwiskoTextbox
             // 
-            this.textBox3.Location = new System.Drawing.Point(305, 287);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(287, 22);
-            this.textBox3.TabIndex = 5;
+            this.NazwiskoTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.NazwiskoTextbox.Location = new System.Drawing.Point(366, 268);
+            this.NazwiskoTextbox.Name = "NazwiskoTextbox";
+            this.NazwiskoTextbox.Size = new System.Drawing.Size(287, 30);
+            this.NazwiskoTextbox.TabIndex = 5;
             // 
-            // textBox4
+            // PeselTextbox
             // 
-            this.textBox4.Location = new System.Drawing.Point(305, 337);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(287, 22);
-            this.textBox4.TabIndex = 6;
+            this.PeselTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PeselTextbox.Location = new System.Drawing.Point(366, 318);
+            this.PeselTextbox.Name = "PeselTextbox";
+            this.PeselTextbox.Size = new System.Drawing.Size(287, 30);
+            this.PeselTextbox.TabIndex = 6;
             // 
-            // LoginLabel
+            // EmailLabel
             // 
-            this.LoginLabel.AutoSize = true;
-            this.LoginLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.LoginLabel.Location = new System.Drawing.Point(49, 137);
-            this.LoginLabel.Name = "LoginLabel";
-            this.LoginLabel.Size = new System.Drawing.Size(130, 17);
-            this.LoginLabel.TabIndex = 7;
-            this.LoginLabel.Text = "Podaj adres e-mail:";
-            this.LoginLabel.Click += new System.EventHandler(this.LoginLabel_Click);
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.EmailLabel.Location = new System.Drawing.Point(110, 128);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(130, 17);
+            this.EmailLabel.TabIndex = 7;
+            this.EmailLabel.Text = "Podaj adres e-mail:";
+            this.EmailLabel.Click += new System.EventHandler(this.LoginLabel_Click);
             // 
-            // label1
+            // ImieLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(49, 235);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 17);
-            this.label1.TabIndex = 8;
-            this.label1.Text = "Podaj imie:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.ImieLabel.AutoSize = true;
+            this.ImieLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.ImieLabel.Location = new System.Drawing.Point(110, 226);
+            this.ImieLabel.Name = "ImieLabel";
+            this.ImieLabel.Size = new System.Drawing.Size(77, 17);
+            this.ImieLabel.TabIndex = 8;
+            this.ImieLabel.Text = "Podaj imie:";
+            this.ImieLabel.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // NazwiskoLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.label2.Location = new System.Drawing.Point(49, 292);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 17);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Podaj nazwisko:";
+            this.NazwiskoLabel.AutoSize = true;
+            this.NazwiskoLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.NazwiskoLabel.Location = new System.Drawing.Point(110, 278);
+            this.NazwiskoLabel.Name = "NazwiskoLabel";
+            this.NazwiskoLabel.Size = new System.Drawing.Size(109, 17);
+            this.NazwiskoLabel.TabIndex = 9;
+            this.NazwiskoLabel.Text = "Podaj nazwisko:";
             // 
-            // label3
+            // PeselLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.label3.Location = new System.Drawing.Point(49, 342);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(96, 17);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Podaj PESEL:";
+            this.PeselLabel.AutoSize = true;
+            this.PeselLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.PeselLabel.Location = new System.Drawing.Point(110, 328);
+            this.PeselLabel.Name = "PeselLabel";
+            this.PeselLabel.Size = new System.Drawing.Size(96, 17);
+            this.PeselLabel.TabIndex = 10;
+            this.PeselLabel.Text = "Podaj PESEL:";
             // 
-            // label4
+            // HasloLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.label4.Location = new System.Drawing.Point(49, 396);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 17);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Podaj PESEL:";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.HasloLabel.AutoSize = true;
+            this.HasloLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.HasloLabel.Location = new System.Drawing.Point(110, 176);
+            this.HasloLabel.Name = "HasloLabel";
+            this.HasloLabel.Size = new System.Drawing.Size(86, 17);
+            this.HasloLabel.TabIndex = 11;
+            this.HasloLabel.Text = "Podaj haslo:";
+            this.HasloLabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // HasloTextbox
+            // 
+            this.HasloTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.HasloTextbox.Location = new System.Drawing.Point(366, 166);
+            this.HasloTextbox.Name = "HasloTextbox";
+            this.HasloTextbox.Size = new System.Drawing.Size(287, 30);
+            this.HasloTextbox.TabIndex = 12;
+            // 
+            // PunktyRekrLabel
+            // 
+            this.PunktyRekrLabel.AutoSize = true;
+            this.PunktyRekrLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.PunktyRekrLabel.Location = new System.Drawing.Point(110, 379);
+            this.PunktyRekrLabel.Name = "PunktyRekrLabel";
+            this.PunktyRekrLabel.Size = new System.Drawing.Size(232, 17);
+            this.PunktyRekrLabel.TabIndex = 14;
+            this.PunktyRekrLabel.Text = "Podaj ilosc punktow rekrutacyjnych:";
+            // 
+            // PunktyRekrTextbox
+            // 
+            this.PunktyRekrTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.PunktyRekrTextbox.Location = new System.Drawing.Point(366, 369);
+            this.PunktyRekrTextbox.Name = "PunktyRekrTextbox";
+            this.PunktyRekrTextbox.Size = new System.Drawing.Size(287, 30);
+            this.PunktyRekrTextbox.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.label6.Location = new System.Drawing.Point(110, 425);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 15;
+            // 
+            // ZarejestrujButton
+            // 
+            this.ZarejestrujButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ZarejestrujButton.Font = new System.Drawing.Font("Segoe UI Semibold", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ZarejestrujButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.ZarejestrujButton.Location = new System.Drawing.Point(315, 435);
+            this.ZarejestrujButton.Name = "ZarejestrujButton";
+            this.ZarejestrujButton.Size = new System.Drawing.Size(196, 67);
+            this.ZarejestrujButton.TabIndex = 16;
+            this.ZarejestrujButton.Text = "Zarejestruj";
+            this.ZarejestrujButton.UseVisualStyleBackColor = false;
+            // 
+            // Button
+            // 
+            this.Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.Button.Font = new System.Drawing.Font("Segoe UI Semibold", 13F, System.Drawing.FontStyle.Bold);
+            this.Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
+            this.Button.Location = new System.Drawing.Point(647, 537);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(141, 61);
+            this.Button.TabIndex = 17;
+            this.Button.Text = "Powrot";
+            this.Button.UseVisualStyleBackColor = false;
             // 
             // Rejestracja
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(800, 496);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.LoginLabel);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(800, 610);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.ZarejestrujButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.PunktyRekrLabel);
+            this.Controls.Add(this.PunktyRekrTextbox);
+            this.Controls.Add(this.HasloTextbox);
+            this.Controls.Add(this.HasloLabel);
+            this.Controls.Add(this.PeselLabel);
+            this.Controls.Add(this.NazwiskoLabel);
+            this.Controls.Add(this.ImieLabel);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.PeselTextbox);
+            this.Controls.Add(this.NazwiskoTextbox);
+            this.Controls.Add(this.ImieTextbox);
+            this.Controls.Add(this.EmailTextbox);
             this.Controls.Add(this.RejestrHeader);
             this.Name = "Rejestracja";
             this.Text = "Dziekanat";
@@ -158,14 +233,20 @@
         #endregion
 
         private System.Windows.Forms.Label RejestrHeader;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label LoginLabel;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox EmailTextbox;
+        private System.Windows.Forms.TextBox ImieTextbox;
+        private System.Windows.Forms.TextBox NazwiskoTextbox;
+        private System.Windows.Forms.TextBox PeselTextbox;
+        private System.Windows.Forms.Label EmailLabel;
+        private System.Windows.Forms.Label ImieLabel;
+        private System.Windows.Forms.Label NazwiskoLabel;
+        private System.Windows.Forms.Label PeselLabel;
+        private System.Windows.Forms.Label HasloLabel;
+        private System.Windows.Forms.TextBox HasloTextbox;
+        private System.Windows.Forms.Label PunktyRekrLabel;
+        private System.Windows.Forms.TextBox PunktyRekrTextbox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ZarejestrujButton;
+        private System.Windows.Forms.Button Button;
     }
 }
