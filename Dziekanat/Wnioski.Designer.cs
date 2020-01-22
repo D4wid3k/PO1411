580aafbc1d6c4eeac22caa30c6c87267b25b4a11
Karol Karcz <kkkkkarol@onet.pl>
2020-01-22 18:05:47 +0100

    zmienilem appconf

--- Dziekanat/Wnioski.Designer.cs
+++ Dziekanat/Wnioski.Designer.cs
@@ -40,19 +40,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 19.8F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 45);
+            this.label1.Size = new System.Drawing.Size(115, 37);
             this.label1.TabIndex = 3;
             this.label1.Text = "Wnioski";
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(43, 140);
+            this.listBox1.Location = new System.Drawing.Point(32, 114);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(455, 276);
+            this.listBox1.Size = new System.Drawing.Size(342, 225);
             this.listBox1.TabIndex = 4;
             // 
             // label2
@@ -60,9 +61,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 10F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.label2.Location = new System.Drawing.Point(39, 114);
+            this.label2.Location = new System.Drawing.Point(29, 93);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(224, 23);
+            this.label2.Size = new System.Drawing.Size(186, 19);
             this.label2.TabIndex = 5;
             this.label2.Text = "Lista przeslanych wnioskow:";
             // 
@@ -71,36 +73,40 @@
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.button2.Location = new System.Drawing.Point(195, 422);
+            this.button2.Location = new System.Drawing.Point(146, 343);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 62);
+            this.button2.Size = new System.Drawing.Size(112, 50);
             this.button2.TabIndex = 13;
             this.button2.Text = "Usun wniosek";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
             this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(248)))), ((int)(((byte)(255)))));
-            this.button1.Location = new System.Drawing.Point(393, 515);
+            this.button1.Location = new System.Drawing.Point(295, 418);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 62);
+            this.button1.Size = new System.Drawing.Size(112, 50);
             this.button1.TabIndex = 14;
             this.button1.Text = "Powrot";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // Wnioski
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(554, 589);
+            this.ClientSize = new System.Drawing.Size(416, 479);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Wnioski";
             this.Text = "Dziekanat";
             this.Load += new System.EventHandler(this.Wnioski_Load);
--- Dziekanat/Wnioski.cs
+++ Dziekanat/Wnioski.cs
@@ -21,5 +21,10 @@ namespace Dziekanat
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
